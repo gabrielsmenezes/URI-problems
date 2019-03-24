4588af443e6a7cb1a4d39b61b80680c24dec5b5e
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-03-24 10:34:58 -0400

    Problem 1257

---
+++ 1257.py
@@ -0,0 +1,20 @@
+casos = int(input())
+
+while casos > 0:
+
+
+	quantidadeDeLinhas = int(input())
+
+	soma = 0
+	
+	while quantidadeDeLinhas > 0:
+		
+
+
+		quantidadeDeLinhas = quantidadeDeLinhas - 1
+
+
+
+
+
+	casos = casos - 1
