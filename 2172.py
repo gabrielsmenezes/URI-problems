f9287c21c85fb63a071373d184811c82ec40f339
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-03-24 10:39:17 -0400

    Problem 2172

---
+++ 2172.py
@@ -0,0 +1,12 @@
+entrada = input().split(' ')
+x = int(entrada[0])
+m = int(entrada[1])
+
+while x != 0 and m != 0:
+	
+	print(x*m)
+
+
+	entrada = input().split(" ")
+	x = int(entrada[0])
+	m = int(entrada[1])
